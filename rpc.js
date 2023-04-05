c82d977558ba799ba35c510eb625fedaeea441cf
Damjan Djermanovic <djermanovicdamjan@gmail.com>
2023-04-05 21:52:13 +0200

    Make playRound function.

--- rpc.js
+++ rpc.js
@@ -1,18 +1,46 @@
 function getComputerChoice(){
     let randomNumber = Math.floor((Math.random() * 3) + 1);
-    let computerChoice = "";
+    let computerSelection = "";
     switch(randomNumber){
         case 1:
-            computerChoice = "Rock";
+            computerSelection = "Rock";
             break;
         case 2:
-            computerChoice = "Paper";
+            computerSelection = "Paper";
             break;
         case 3:
-            computerChoice = "Scissors"
+            computerSelection = "Scissors"
             break;
     }
-    return computerChoice;
+    return computerSelection;
 }
 
-console.log(getComputerChoice());
+let computerSelection = getComputerChoice()
+let playerSelection = prompt("Select rock, paper or scissors!")
+
+function playRound(computerSelection, playerSelection){
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+    let result = "";
+
+    if (player === computer){
+        result = "Draw!";
+    }
+    else if (player === "rock"){
+        (computer === "scissors") ? result = "You Win! Rock beats Scissors" : result = "You Lose! Paper beats Rock";
+    }
+    else if (player === "paper"){
+        (computer === "rock") ? result = "You Win! Paper beats Rock" : result = "You Lose! Scissors beats Paper";
+    }
+    else if (player === "scissors"){
+        (computer === "paper") ? result = "You Win! Scissors beats Paper" : result = "You Lose! Rock beats Scissors";
+    }
+    else {
+        result = "Invalid input";
+    }
+    return result;
+}
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(computerSelection, playerSelection));
